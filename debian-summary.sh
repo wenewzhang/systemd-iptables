9e3341018e1e897ff318e50d9ce150c923510142
Wenew Zhang <wenewboy@gmail.com>
2022-09-01 07:49:12 +0800

    client & port

--- debian-summary.sh
+++ debian-summary.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /var/log/daemon.log |grep ss-server|grep Accepting|grep -v message|awk '{print $5,$12}'|sort|uniq
+cat /var/log/daemon.log |grep ss-server|grep Accepting|grep -v message|awk '{print $5,$12,$15}'|sort|uniq
