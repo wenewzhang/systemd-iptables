57cea2d774bd1b8c54f4247895f65cdd86d967e6
Wenew Zhang <wenewboy@gmail.com>
2022-09-01 08:28:56 +0800

    remove -v message

--- ubuntu-summary.sh
+++ ubuntu-summary.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /var/log/syslog |grep ss-server|grep Accepting|grep -v message|awk '{print $5,$12,$15}'|sort|uniq
+cat /var/log/syslog |grep ss-server|grep Accepting|awk '{print $5,$12,$15}'|sort|uniq
