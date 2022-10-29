d8e8c9270de6ae2b80d11883f0c3af7685fa175f
Wenew Zhang <wenewboy@gmail.com>
2022-10-29 11:27:00 +0800

    fixZ

--- usr/bin/whitelist.sh
+++ usr/bin/whitelist.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 ipset create whitelist hash:net counters comment maxelem 1000000
-iptables -I INPUT -p tcp --dport 22  -j ACCEPT
+iptables -P INPUT DROP
+iptables -A INPUT -p tcp --dport 22  -j ACCEPT
+# Below 1 line need by wget/apt for dns
+iptables -A INPUT -p udp --dport 53  -j ACCEPT
+# Below 1 line Need by shadowsocks data packages transfer
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT  -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT DROP
-ipset add whitelist 113.218/16
+ipset add whitelist 113.218.0.0/16
+
+
+##  iptables -L INPUT -v -n 
