935f32b120dc4e2eb88c7119b8e082c602a38a62
Wenew Zhang <wenewboy@gmail.com>
2022-10-30 19:50:33 +0800

    add ip

---
+++ add-ip-to-whitelist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+[ -z "$1" ] && { echo "./add-ip-to-whitelist.sh <ip>"; exit; }
+Find=$(ipset -L |grep $1)
+[ -z "$Find" ] && { echo "This ip already in ipset! ($1)"; exit; }
+ipset add whitelist $1
+echo $1 >> whitelist.txt
