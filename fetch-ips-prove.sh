82f1746cdbbd0054e7bde1b20c3c532b420e8b57
Wenew Zhang <wenewboy@gmail.com>
2022-11-30 20:19:08 +0800

    fix

--- fetch-ips-prove.sh
+++ fetch-ips-prove.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat /var/log/daemon.log |grep auth |awk '{print $13}'|awk -F. '{print $1"."$2"."$3".0/"24}'|sort|uniq > _blacklist.txt
-cat /var/log/daemon.log.1 |grep auth |awk '{print $13}'|awk -F. '{print $1"."$2"."$3".0/"24}'|sort|uniq >> _blacklist.txt
+cat /var/log/daemon.log |grep auth |awk '{print $13}'|awk -F. '{print $1"."$2"."$3}'|sort|uniq > _blacklist.txt
+cat /var/log/daemon.log.1 |grep auth |awk '{print $13}'|awk -F. '{print $1"."$2"."$3}'|sort|uniq >> _blacklist.txt
 cat _blacklist.txt | sort | uniq >> blacklist.txt
