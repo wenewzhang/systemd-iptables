5eeb4a71d3432a4d460126e6ca9e302ac669c949
Wenew Zhang <wenewboy@gmail.com>
2024-05-24 19:40:27 +0800

    fix

--- install-dailyclean.sh
+++ install-dailyclean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp ./usr/bin/daily-clean.sh /usr/bin/.
 chmod +x /usr/bin/daily-clean.sh
-crontab -l | { cat; echo "* 3 * * *     /usr/bin/daily-cleanup.sh"; } | crontab -
+crontab -l | { cat; echo "* 3 * * *     /usr/bin/daily-clean.sh"; } | crontab -
