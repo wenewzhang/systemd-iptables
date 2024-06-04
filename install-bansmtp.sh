3ca9f3f0d4034bdfbbffb3fed7559b042a57a455
Wenew Zhang <wenewboy@gmail.com>
2024-06-04 10:00:54 +0800

    fix

--- install-bansmtp.sh
+++ install-bansmtp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp ./usr/bin/ban-smtp.sh /usr/bin/.
+cp ./usr/bin/allow-smtp.sh /usr/bin/.
 chmod +x /usr/bin/allow-smtp.sh
 chmod +x /usr/bin/ban-smtp.sh
 cp -r ./etc/systemd/system/bansmtp.service /etc/systemd/system/
