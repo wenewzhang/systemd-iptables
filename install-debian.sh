63ebeaabe84a6ae87a33105af6f79f804625ad51
Wenew Zhang <wenewboy@gmail.com>
2024-11-27 19:32:25 +0800

    remove traffic

--- install-debian.sh
+++ install-debian.sh
@@ -2,7 +2,7 @@
 apt install git vnstat net-tools iftop shadowsocks-libev fail2ban  ipset -y
 systemctl disable shadowsocks-libev --now
 ./install-whitelist.sh
-./install-cron.sh
+# ./install-cron.sh
 ./install-ipt.sh
 ./opt-tcp-files.sh
 ./install-bansmtp.sh
