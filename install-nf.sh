e43a093acdd84ccef4b73916627c13c005e54832
Wenew Zhang <wenewboy@gmail.com>
2024-12-18 13:40:52 +0800

    fix

--- install-nf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp usr/bin/nf_no_smtp.sh /usr/bin/.
-chmod +x /usr/bin/nf_no_smtp.sh
-chmod +x /usr/bin/nf_no_smtp.sh
-cp -r etc/systemd/system/bansmtp-nf.service /etc/systemd/system/
-systemctl enable bansmtp-nf.service
-systemctl start bansmtp-nf.service
