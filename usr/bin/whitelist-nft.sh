8c934f53f71e4cdaf5c21ebf4ac1ab1747654d92
Wenew Zhang <wenewboy@gmail.com>
2024-12-25 09:13:40 +0800

    add blacklist

--- usr/bin/whitelist-nft.sh
+++ usr/bin/whitelist-nft.sh
@@ -3,3 +3,7 @@
 while read -r ip; do
   /usr/sbin/nft add element ip filter whitelist { $ip }
 done < /etc/whitelist-http/whitelist.txt
+
+while read -r ip; do
+  /usr/sbin/nft add element ip filter blacklist { $ip }
+done < /etc/whitelist-http/blacklist.txt
